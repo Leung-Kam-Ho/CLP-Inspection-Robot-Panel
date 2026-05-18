67cd8cd3251d025f83b94b2c6f79532956207555
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-05-18 18:48:09 +0800

    Add UserView component with basic SwiftUI structure

---
+++ CLP_Inspection_Robot_Panel/MainView/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  UserView.swift
+//  CLP_Inspection_Robot_Panel
+//
+//  Created by KamHo on 18/5/26.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserView()
+}
